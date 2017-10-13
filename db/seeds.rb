44f7243dbef255f7ee6e54bd92da3ac40e02119d
Kynan <k.song@blueyonder.co.uk>
2017-10-13 11:28:43 +0100

    Added second customer to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@ require_relative( '../models/tickets.rb')
 
 customer_1 = Customer.new({'name' => 'Claire', 'funds' => 30})
 
+customer_2 = Customer.new({'name' => 'Callum', 'funds' => 20})
+
 binding.pry
 nil
