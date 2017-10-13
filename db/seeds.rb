5043dd751e7f013c228da3fe13e9621fabdf1e38
Kynan <k.song@blueyonder.co.uk>
2017-10-13 11:07:13 +0100

    Created a customer in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@ require( 'pry' )
 require_relative( '../models/customer.rb')
 require_relative( '../models/films.rb')
 require_relative( '../models/tickets.rb')
+
+customer_1 = Customer.new({'name' => 'Claire', 'funds' => 30})
+
+binding.pry
+nil
--- models/customer.rb
+++ models/customer.rb
@@ -9,7 +9,7 @@ class Customer
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
-    @funds = options['funds']
+    @funds = options['funds'].to_i
   end
 
 end
