4a2bb51bc3ea93e6a16936084a7214e9f245e9cc
Kynan <k.song@blueyonder.co.uk>
2017-10-13 11:16:33 +0100

    Save function added to customer.rb.

--- models/customer.rb
+++ models/customer.rb
@@ -12,4 +12,13 @@ class Customer
     @funds = options['funds'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO customers (name, funds)
+           VALUES ($1, $2)
+           RETURNING * "
+    values = [@name, @funds]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
+
 end
