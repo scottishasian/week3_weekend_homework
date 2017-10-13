925562c82f1d8bb7e8ee2760747ba450ce2e10db
Kynan <k.song@blueyonder.co.uk>
2017-10-13 15:22:22 +0100

    Added delete, all and find functions to tickets.rb

--- models/tickets.rb
+++ models/tickets.rb
@@ -21,4 +21,35 @@ class Tickets
     @id = results[0]['id'].to_i
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql "DELETE FROM tickets
+         WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    values = []
+    results = SqlRunner.run(sql, values)
+    tickets_as_objects = results.map{|ticket| Tickets.new(ticket)}
+    return tickets_as_objects
+  end
+
+  def self.find(ticket_id)
+    sql = "SELECT * FROM tickets
+           WHERE id = $1"
+    values = [ticket_id]
+    result = SqlRunner.run(sql, values)
+    ticket_details = result[0]
+    ticket_info = Tickets.new(ticket_details)
+    return ticket_info
+  end
+
 end
